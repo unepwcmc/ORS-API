475387219d0a2184c50397768d7e222375f09788
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-10-20 14:35:01 +0100

    text end-point for exception notifier

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::BaseController < ApplicationController
+
+  # this end-point to be used to test exception notifier
+  def test_exception_notifier
+    raise 'This is a test. This is only a test.'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :questionnaires, :only => [:index]
     end
+    get 'test_exception_notifier', controller: :base, action: :test_exception_notifier
   end
 
   apipie
