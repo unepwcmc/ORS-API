e1d5e2e6fd14e3855d8bd21e57c4e07801e10c24
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-10-20 12:48:58 +0200

    Allow single access token requests for GET

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   acts_as_authentic do |c|
     c.login_field = 'email'
   end
+
+  def generate_authentication_token
+    token = loop do
+      t = SecureRandom.base64.tr('+/=', 'Qrt')
+      break t unless User.exists?(single_access_token: t)
+    end
+    self.update_attribute(:single_access_token, token)
+  end
 end
