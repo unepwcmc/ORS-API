088b62e951c608b28394c43612ada802e4198e79
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-10-20 18:09:29 +0200

    Fix unpermitted name parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
   private
 
   def users_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :first_name, :last_name)
   end
 end
