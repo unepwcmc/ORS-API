3ba64064576bf00f4b461b08046e3e6950d64a39
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-10-21 11:44:32 +0100

    removed trailing spaces

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,7 +6,7 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      redirect_to root_path 
+      redirect_to root_path
     else
       render :action => :new
     end
--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,3 +1,3 @@
 class UserSession < Authlogic::Session::Base
   single_access_allowed_request_types [:get]
-end  
+end
