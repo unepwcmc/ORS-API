8fe921fcbe9cca958d9e5be9b1ff4740723162cf
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-10-21 11:49:44 +0100

    amended test for access restriction

--- test/controllers/api/v1/questionnaires_controller_test.rb
+++ test/controllers/api/v1/questionnaires_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class Api::V1::QuestionnairesControllerTest < ActionController::TestCase
-  test "should respond with success" do
+  test "should respond with 401" do
     get :index
-    assert_response :success
+    assert_response 401
   end
 end
