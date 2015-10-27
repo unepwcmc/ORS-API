89a4f3f066f131cabbe528d1c68c5e7f35e04bcf
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-10-27 17:44:55 +0000

    Added language parameter

--- app/controllers/api/v1/questionnaire_details_controller.rb
+++ app/controllers/api/v1/questionnaire_details_controller.rb
@@ -13,11 +13,23 @@ class Api::V1::QuestionnaireDetailsController < Api::V1::BaseController
   param :id, String,
     desc: 'Id of the questionnaire',
     required: true 
+  param :lng, String,
+    desc: 'Where available display data in language given by ISO code (e.g. "EN"). Defaults to questionnaire\'s default language.'
 
   def show
     @questionnaire = Questionnaire.
       includes(:respondents).references(:respondents).
-      where(status: ['Active', 'Closed']).find(params[:id])
+      where(status: ['Active', 'Closed']).where(id: params[:id])
+    @questionnaire = if @language
+      @questionnaire.
+      where(
+        "languages @> ARRAY[:language] AND language = :language
+        OR NOT languages @> ARRAY[:language] AND is_default_language",
+        language: @language
+      )
+    else
+      @questionnaire.where(is_default_language: true)
+    end.first
     respond_with @questionnaire
   end
 
