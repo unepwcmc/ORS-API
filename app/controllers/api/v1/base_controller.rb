f4a063fc20a203b9ff93298cb94fb86945052941
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-11-12 10:44:08 +0000

    removed environment check when handling exceptions

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -46,14 +46,13 @@ class Api::V1::BaseController < ApplicationController
   end
 
   def return_exception_error(exception)
-    if Rails.env.production? || Rails.env.staging?
-      ExceptionNotifier.notify_exception(exception,
-        env: request.env, data: { message: "Something went wrong" }
-      )
-    else
-      Rails.logger.error exception.message
-      Rails.logger.error exception.backtrace.join("\n")
-    end
+    ExceptionNotifier.notify_exception(exception,
+      env: request.env, data: { message: "Something went wrong" }
+    )
+
+    Rails.logger.error exception.message
+    Rails.logger.error exception.backtrace.join("\n")
+
     render json: { message: "We are sorry, but something went wrong while processing your request" }, status: 500
   end
 
--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,44 +1,42 @@
-if Rails.env.production? || Rails.env.staging?
-  require 'exception_notification/rails'
+require 'exception_notification/rails'
 
-  ExceptionNotification.configure do |config|
-    # Ignore additional exception types.
-    # ActiveRecord::RecordNotFound, AbstractController::ActionNotFound and ActionController::RoutingError are already added.
-    # config.ignored_exceptions += %w{ActionView::TemplateError CustomError}
+ExceptionNotification.configure do |config|
+  # Ignore additional exception types.
+  # ActiveRecord::RecordNotFound, AbstractController::ActionNotFound and ActionController::RoutingError are already added.
+  # config.ignored_exceptions += %w{ActionView::TemplateError CustomError}
 
-    # Adds a condition to decide when an exception must be ignored or not.
-    # The ignore_if method can be invoked multiple times to add extra conditions.
-    config.ignore_if do |exception, options|
-      not (Rails.env.production? || Rails.env.staging?)
-    end
-
-    # Notifiers =================================================================
-
-    # Email notifier sends notifications by email.
-    config.add_notifier :email, {
-      email_prefix:         "[ORS-API #{Rails.env}] ",
-      sender_address:       %{"Exception Notification" <no-reply@unep-wcmc.org>},
-      exception_recipients: Rails.application.secrets.exception_notification_email
-    }
-
-    # Campfire notifier sends notifications to your Campfire room. Requires 'tinder' gem.
-    # config.add_notifier :campfire, {
-    #   :subdomain => 'my_subdomain',
-    #   :token => 'my_token',
-    #   :room_name => 'my_room'
-    # }
-
-    # HipChat notifier sends notifications to your HipChat room. Requires 'hipchat' gem.
-    # config.add_notifier :hipchat, {
-    #   :api_token => 'my_token',
-    #   :room_name => 'my_room'
-    # }
-
-    config.add_notifier :slack, {
-      webhook_url: Rails.application.secrets.slack_exception_notification_webhook_url,
-      channel: "#online-reporting-tool",
-      username: "TheTormentingBotOfORS-API-#{Rails.env}"
-    }
-
+  # Adds a condition to decide when an exception must be ignored or not.
+  # The ignore_if method can be invoked multiple times to add extra conditions.
+  config.ignore_if do |exception, options|
+    not (Rails.env.production? || Rails.env.staging?)
   end
+
+  # Notifiers =================================================================
+
+  # Email notifier sends notifications by email.
+  config.add_notifier :email, {
+    email_prefix:         "[ORS-API #{Rails.env}] ",
+    sender_address:       %{"Exception Notification" <no-reply@unep-wcmc.org>},
+    exception_recipients: Rails.application.secrets.exception_notification_email
+  }
+
+  # Campfire notifier sends notifications to your Campfire room. Requires 'tinder' gem.
+  # config.add_notifier :campfire, {
+  #   :subdomain => 'my_subdomain',
+  #   :token => 'my_token',
+  #   :room_name => 'my_room'
+  # }
+
+  # HipChat notifier sends notifications to your HipChat room. Requires 'hipchat' gem.
+  # config.add_notifier :hipchat, {
+  #   :api_token => 'my_token',
+  #   :room_name => 'my_room'
+  # }
+
+  config.add_notifier :slack, {
+    webhook_url: Rails.application.secrets.slack_exception_notification_webhook_url,
+    channel: "#online-reporting-tool",
+    username: "TheTormentingBotOfORS-API-#{Rails.env}"
+  }
+
 end
