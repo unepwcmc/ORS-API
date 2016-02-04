98cf10bd533a3898baa6063c9eb50d93a4a498af
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-02-04 15:05:38 +0000

    Fix failing test changing the scope of Question

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -15,7 +15,7 @@ class QuestionTest < ActiveSupport::TestCase
       @user2 = FactoryGirl.create(:user)
       @answer2 = FactoryGirl.create(:answer, question: @question, user: @user2)
       FactoryGirl.create(:answer_part, answer: @answer2)
-      @question_from_view = Question.find(@question.id)
+      @question_from_view = Test::Question.find(@question.id)
     end
     it "should have answers" do
       assert @question_from_view.answers.count == 2
