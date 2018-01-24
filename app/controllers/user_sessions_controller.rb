864b5f47b0c8b0cc209b75bc932ce89616402a2e
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-01-24 10:03:15 +0000

    Use strong parameters. Required for Authlogic.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,7 +5,7 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    @user_session = UserSession.new(params[:user_session])
+    @user_session = UserSession.new(session_params)
     if @user_session.save
       redirect_to root_path
     else
@@ -16,5 +16,11 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     redirect_to new_user_session_url
+  end
+
+  private
+
+  def session_params
+    params.require(:user_session).permit(:email, :password)
   end
 end
